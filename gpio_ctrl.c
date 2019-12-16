c55a76f519423981f927d01f21caa67338fdb746
Nick Nagy <nickvincentnagy@gmail.com>
2019-12-15 20:00:59 -0800

    redesign of file and macros

--- gpio_ctrl.c
+++ gpio_ctrl.c
@@ -4,17 +4,18 @@ Setting GPIO clocks using BCM2835 ARM Peripherals Guide.
 
 #include "gpio_ctrl.h"
 
-bool clocksInitialized = 0;
-int sourceFreq;
+static char clocksInitialized;
+static char clocksRunning;
+static char clockSource;
+static unsigned clockSourceFreq;
 unsigned int * gpio;
 unsigned int * clk_ctrl;
 
-static int getSourceFrequency() {
-    switch (SRC) {
+static int getSourceFrequency(char src) {
+    switch (src) {
         case OSC: return OSC_FREQ;
         case PLLC: return PLLC_FREQ;
         case PLLD: return PLLD_FREQ;
-        case HDMI: return HDMI_FREQ;
         default: return 0;
     }
 }
@@ -23,54 +24,136 @@ static bool clockIsBusy(int clock_ctrl_reg) {
     return (((clock_ctrl_reg)>>7) & 1);
 }
 
-int setDiv(unsigned int freq) {
-    float diviFloat = (float)sourceFreq / freq;
+int setDiv(unsigned freq, bool mash) {
+    float diviFloat = (float)clockSourceFreq / freq;
     int divi = (int)diviFloat;
-    if (MASH) 
+    if (mash) 
         return (divi << 12) | (int)(4096 * (diviFloat - divi));
     return divi << 12;
 }
 
-void setPinHigh(unsigned char n) {
+void setPinHigh(char n) {
     gpio[GPIO_SET_REG] |= (1 << n);
 }
 
-void setPinLow(unsigned char n) {
+void setPinLow(char n) {
     gpio[GPIO_CLR_REG] |= (1 << n);
 }
 
-static void initClocks() {
-    if (!clocksInitialized) {
-        gpio[0] |= CLK0_FSEL_BITS;
-        // disable clock first
-        while(clk_ctrl[CLK0_CTRL_REG] & BUSY) {
-            clk_ctrl[CLK0_CTRL_REG] = (CLK_PASSWD | KILL); //& ~ENABLE);
+static bool isValidClockSelection(char clockNum) {
+    switch(clockNum) {
+        case 0: {
+            printf("Clock 0 is available for user\n");
+            return 1;
         }
-        clk_ctrl[CLK0_DIV_REG] = (CLK_PASSWD | setDiv(MCLK_FREQ));
-        usleep(10);
-        clk_ctrl[CLK0_CTRL_REG] = CLK_CTRL_INIT_BITS;
-        usleep(10);
-        clk_ctrl[CLK0_CTRL_REG] |= (CLK_PASSWD | ENABLE);
-        printf("clock 0 is set to run @ %d Hz. ctrl reg @ %p = %x, div reg @ %p = %x\n", MCLK_FREQ, &clk_ctrl[CLK0_CTRL_REG], clk_ctrl[CLK0_CTRL_REG], &clk_ctrl[CLK0_DIV_REG], clk_ctrl[CLK0_DIV_REG]);
-        usleep(10);
-        clk_ctrl[CLK0_CTRL_REG] |= (CLK_PASSWD | KILL);
-        clocksInitialized = 1;
+        case 1: { // TODO: add model dependencies
+            printf("Clock 1 is not available for user.\n");
+            return 0;
+        }
+        case 2: { // TODO: add model dependencies
+            printf("Clock 2 is available for user\n"); 
+            return 1;
+        }
+        default: {
+            printf("Invalid clock.\n");
+            return 0;
+        }
     }
 }
 
-int main() {
-    unsigned int bcm_base = bcm_host_get_peripheral_address();
-    int fdgpio = open("/dev/mem", O_RDWR | O_SYNC);//("/dev/gpiomem", O_RDWR);
-    if (fdgpio < 0) {
-        printf("Failure to access /dev/\n"); 
+void disableClock(char clockNum) {
+    while(clk_ctrl[CLK_CTRL_REG(clockNum)] & BUSY) {
+        clk_ctrl[CLK_CTRL_REG(clockNum)] = (CLK_PASSWD & ~ENABLE);
+    }
+    clocksRunning ^= (1 << clockNum);
+}
+
+static int initClockRegs(char clockNum, unsigned frequency, bool mash) {
+    if (!isValidClockSelection(clockNum)) return 1;
+    gpio[clockNum] |= CLK_FSEL_BITS(clockNum);
+    if((clocksRunning >> clockNum) & 1) 
+        disableClock(clockNum);
+    clk_ctrl[CLK_DIV_REG(clockNum)] = (CLK_PASSWD | setDiv(frequency, mash));
+    usleep(10);
+    clk_ctrl[CLK_CTRL_REG(clockNum)] = CLK_PASSWD | MASH(mash) | clockSource;
+    usleep(10);
+    printf("clock %d is set to run @ %d Hz. ctrl reg @ %p = %x, div reg @ %p = %x\n", clockNum, frequency, &clk_ctrl[CLK_CTRL_REG(clockNum)], clk_ctrl[CLK_CTRL_REG(clockNum)], &clk_ctrl[CLK_DIV_REG(clockNum)], clk_ctrl[CLK_DIV_REG(clockNum)]);
+    clocksInitialized |= 1 << clockNum;
+    return 0;
+}
+
+static int startClock(char clockNum) {
+    if ((clocksRunning >> clockNum) & 1) {
+        printf("Clock %d is already running. You will have to disable it first.\n", clockNum);
         return 1;
     }
-    clk_ctrl = (unsigned int *)mmap((unsigned int*)(bcm_base + CLK_CTRL_BASE_OFFSET), CLK_CTRL_BASE_PAGESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fdgpio, 0);
-    gpio = (unsigned int *)mmap((unsigned int*)(bcm_base + GPIO_BASE_OFFSET), GPIO_BASE_PAGESIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fdgpio, 0);
-    sourceFreq = getSourceFrequency();
-    printf("running system off of source #%d at frequency %dHz\n", SRC, sourceFreq);
-    initClocks();
-    munmap(gpio, GPIO_BASE_PAGESIZE);
-    munmap(clk_ctrl, CLK_CTRL_BASE_PAGESIZE);
+    if (!((clocksInitialized >> clockNum) & 1)) {
+        printf("ERROR: clock %d hasn't been initialized.\n", clockNum);
+        return 1;
+    }
+    if (VERBOSE) {
+        unsigned divi = (clk_ctrl[CLK_DIV_REG(clockNum)] >> 12) & 0xFFF;
+        unsigned divf = clk_ctrl[CLK_DIV_REG(clockNum)] & 0xFFF; 
+        unsigned clockFreq = (unsigned) clockSourceFreq / (divi + (divf/4096.0));
+        printf("Starting clock %d @ %d Hz...\n", clockNum, clockFreq);
+    }
+    clocksRunning |= (1 << clockNum);
+    clk_ctrl[CLK_CTRL_REG(clockNum)] |= (CLK_PASSWD | ENABLE);
+    return 0;
+}
+
+void LEDTest(char pin, unsigned char numBlinks, unsigned delay_seconds) {
+    unsigned delay_us = 1000000 * delay_seconds;
+    printf("FSEL clear = %x\n", FSEL_CLEAR_REG(pin%10));
+    gpio[pin/10] &= FSEL_CLEAR_REG(pin%10) | (1 << FSEL_SHIFT(pin%10));
+    if (VERBOSE) printf("GPIO FSEL%d = %x\n", pin/10, gpio[pin/10]);
+    for (char i = 0; i < numBlinks; i++) {
+        printf("%d\n",i);
+        setPinHigh(pin);
+        usleep(delay_us);
+        setPinLow(pin);
+        usleep(delay_us);
+    }
+}
+
+static bool initMemMap() {
+    unsigned bcm_base = bcm_host_get_peripheral_address();
+    int fd = open("/dev/mem", O_RDWR | O_SYNC);
+    if (fd < 0) {
+        printf("Failure to access /dev/mem\n"); 
+        return 0;
+    }
+    clk_ctrl = (unsigned *)mmap((unsigned *)(bcm_base + CLK_CTRL_BASE_OFFSET), CLK_CTRL_BASE_MAPSIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("clock control and div registers loaded from /dev/mem.\n");
+    gpio = (unsigned *)mmap((unsigned *)(bcm_base + GPIO_BASE_OFFSET), GPIO_BASE_MAPSIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+    printf("GPIO loaded from /dev/mem.\n");
+    return 1;
+}
+
+static void clearMemMap() {
+    munmap(gpio, GPIO_BASE_MAPSIZE);
+    munmap(clk_ctrl, CLK_CTRL_BASE_MAPSIZE);
+}
+
+void disableAllClocks() {
+    char c = clocksInitialized;
+    for (int i = 0; i < 3; i++) {
+        if ((clocksRunning >> i) & 1)
+            disableClock(i);
+    }
+    clocksInitialized = 0;
+}
+
+int main(int argc, char** argv) {
+    if(!initMemMap()) return 1;
+    clockSource = OSC;
+    clockSourceFreq = getSourceFrequency(clockSource);
+    printf("running system off of source #%d at frequency %d Hz\n", clockSource, clockSourceFreq);
+    clocksRunning = 0xFF;
+    initClockRegs(0, 11289000, 1);
+    startClock(0);
+    LEDTest(8, 0, 1);
+    disableAllClocks();
+    clearMemMap();
     return 0;
 }
--- gpio_ctrl.h
+++ gpio_ctrl.h
@@ -13,6 +13,11 @@ Setting GPIO clocks using BCM2835 ARM Peripherals Guide.
 #include <sys/mman.h>
 #include <bcm_host.h>
 
+#define FSEL_SHIFT(x) 3*x
+#define FSEL_CLEAR_REG(x) (7 << FSEL_SHIFT(x)) ^ 0xFFFFFFFF
+
+#define VERBOSE 1
+
 /* 
 ******************** CLOCK CONTROL ************************
 Registers and relevant bit assignments for setting up GPCLK
@@ -21,114 +26,70 @@ Registers and relevant bit assignments for setting up GPCLK
 // function select register for GPCLK0, GPCLK1, GPCLK2
 // clock control registers start at +0x101070, but this is not a multiple of our system page size, so we have to start from +0x101000
 #define CLK_CTRL_BASE_OFFSET 0x101000 
-#define CLK_CTRL_BASE_PAGESIZE 136 // need to hold up to 0x101088
+#define CLK_CTRL_BASE_MAPSIZE 136 // need to hold up to 0x101088
 
-// input for *GPFSEL0. Sets GPCLKs to their clock function
-#define CLK0_FSEL_BITS (4 << 4)
-#define CLK1_FSEL_BITS (4 << 5)
-#define CLK2_FSEL_BITS (4 << 6)
-#define CLK_FSEL_BITS (CLK0_FSEL_BITS) //| CLK1_FSEL_BITS | CLK2_FSEL_BITS)
+// function select
+#define CLK_ALT_SHIFT(x) 12 + FSEL_SHIFT(x)
+#define CLK_FSEL_BITS(x) 4 << CLK_ALT_SHIFT(x)
 
-#define CLK0_CTRL_REG 28
-#define CLK0_DIV_REG  29
-#define CLK1_CTRL_REG 30
-#define CLK1_DIV_REG  31
-#define CLK2_CTRL_REG 32
-#define CLK2_DIV_REG  33
+// ctrl and div
+#define CLK_CTRL_REG(x) 28 + (x<<1)
+#define CLK_DIV_REG(x) 29 + (x<<1)
 
-// source frequencies
-#define OSC_FREQ 19200000
-#define TESTDEBUG0_FREQ 0
-#define TESTDEBUG1_FREQ 0
-#define PLLA_FREQ 0
-#define PLLC_FREQ 1000000000
-#define PLLD_FREQ 500000000
-#define HDMI_FREQ 216000000
-
-// relevant clock frequencies
-#define MCLK_FREQ 11289000
-#define MCLK_LRCLK_RATIO 256 // =(2^val). may have to be empirically determined
-#define PCM_FS_FREQ (MCLK_FREQ / (2*MCLK_LRCLK_RATIO))
-#define SCLK_FREQ PCM_FS_FREQ*DATA_SIZE
-
-#define CLK_PASSWD (0x5A << 24) //000000
-#define MASH (1 << 9)
+#define CLK_PASSWD (0x5A << 24)
+#define MASH(x) (x << 9)
 #define BUSY (1 << 7)
 #define KILL (1 << 5)
 #define ENABLE (1 << 4)
 
+// source frequencies
+#define OSC_FREQ 19200000
+#define PLLC_FREQ 1000000000
+#define PLLD_FREQ 500000000
+#define HDMI_FREQ 216000000
+
 // sources
 #define OSC 1
-#define TESTDEBUG0 2
-#define TESTDEBUG1 3
 #define PLLA 4
 #define PLLC 5
 #define PLLD 6
-#define HDMI 7
-
-#define SRC OSC
-#define CLK_CTRL_INIT_BITS (CLK_PASSWD | MASH | SRC)
-
-/*
-*********************** I2S CONTROL ************************
-Registers and relevant bit assignments for setting up I2S
-*/
-
-#define I2S_BASE_OFFSET 0x203000 //BCM_BASE + 0x203000
-#define I2S_PAGESIZE 24
-
-// register indices in I2S memory map array (FIFO_REG @ I2S_BASE + 0x4, MODE_REG @ I2S_BASE + 0x8, etc)
-#define CTRL_STATUS_REG 0
-#define FIFO_REG 1
-#define MODE_REG 2
-#define RX_CONFIG_REG 3
-#define TX_CONFIG_REG 4
-#define DREQ_LVL_REG 5
-#define INTERRUPT_EN_REG 6
-#define INTERRUPT_STATUS_REG 7
-#define GRAY_MODE_REG 8 
-
-// input for FSEL, set GPIO 18 - 21 to I2S function
-#define PCM_CLK_FSEL_BITS 0x24000000 // for FSEL0
-#define PCM_DATA_FSEL_BITS 0x24 // for FSEL1 
-
-// MODE REGISTER BITS
-#define FRAME_SYNC_MODE 1 // 0 = master (we generate frame sync), 1 = slave (frame sync is an input) 
-#define FSLEN 2*DATA_SIZE // irrelevant when FRAME_SYNC_MODE == 0
-#define FLEN 2*FSLEN // irrelevant when FRAME_SYNC_MODE == 0
-#define MODE_REG_BITS (FRAME_SYNC_MODE << 21) | (FLEN<<10) | FSLEN
-
-// TX & RX BITS
-# define CH1POS 1 // PMOD MSB bit is read after first complete SCLK cycle when LRCLK changes states
-# define CH1WID 0 // total bits = 8 + CHWID + (CHWEX * 16)
-# define CH2WID CH1WID
-# define CH2POS CH1POS + CH1WID
-# define RX_CONFIG_REG_BITS (1<<30) | (CH1POS << 20) | (CH1WID << 16) | (1 << 14) | (CH2POS << 4) | (CH2WID) // reg[30] & reg[14] are enable bits
-# define TX_CONFIG_REG_BITS RX_CONFIG_REG_BITS
 
 /*
 ********************** GPIO CONTROL ************************
 All other GPIO-relevant registers and bit assignments
 */
 
-// gpio
 #define GPIO_BASE_OFFSET 0x200000//(BCM_BASE + 0x200000)// 0x7E200000
-#define GPIO_BASE_PAGESIZE 4096
+#define GPIO_BASE_MAPSIZE 244
 
 #define GPIO_SET_REG 7  // turns GPIO pins high
 #define GPIO_CLR_REG 10 // turns GPIO pins low
 #define GPIO_LEV_REG 13 // when in input mode, bit n reads GPIO pin n
 
-static int getSourceFrequency();
+static int getSourceFrequency(char src);
 
 static bool clockIsBusy(int clock_ctrl_reg);
 
-void setPinHigh(unsigned char n);
+void setPinHigh(char n);
 
-void setPinLow(unsigned char n);
+void setPinLow(char n);
 
-int setDiv(unsigned int freq);
+int setDiv(unsigned freq, bool mash);
 
-static void initClocks();
+static bool isValidClockSelection(char clockNum);
+
+void disableClock(char clockNum);
+
+static int startClock(char clockNum);
+
+static int initClockRegs(char clockNum, unsigned frequency, bool mash);
+
+void disableAllClocks();
+
+void LEDTest(char pin, unsigned char numBlinks, unsigned delay_seconds);
+
+static bool initMemMap();
+
+static void clearMemMap();
 
 #endif
