b655fa1f8be6fdc129bea266cf7c3cc727f0bdba
Nick <nickvincentnagy@gmail.com>
2019-12-29 21:35:12 -0800

    init getAlignedPtr func

--- globals.h
+++ globals.h
@@ -2,7 +2,7 @@
 #define GLOBALS_H
 
 // for debugging purposes
-#define DEBUG 0
+#define DEBUG 1
 #define VERBOSE 0
 
 #define PRINT_REG(name, reg) printf("%s at address %p = %x\n", name, &reg, reg);
@@ -12,5 +12,7 @@
 #define PRINT_VAL(name, val) printf("%s = %d\n", name, val);
 #define COND_PRINT_VAL(cond, name, val) if (cond) PRINT_VAL(name, val);
 #define DEBUG_VAL(name, val) COND_PRINT_VAL(DEBUG, name, val)
+
+void * getAlignedPointer(void * ptr, int byteAlignment);
 
 #endif
