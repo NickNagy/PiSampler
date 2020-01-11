ea9fa32d050093f4268dd348877ef192ee76dac7
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2020-01-11 12:30:02 -0800

    start of virtual to physical mapping

--- bcm/pimem.h
+++ bcm/pimem.h
@@ -8,8 +8,24 @@
 
 #define BCM_PAGESIZE 4096
 
-volatile unsigned * initMemMap(unsigned offset, unsigned size);
-void clearMemMap(unsigned * map, unsigned size);
-int getPhysAddrBase();
+#define L2_COHERENT_BASE 0x40000000
+#define DIRECT_UNCACHED_BASE 0xC0000000
+
+#define PAGEMAP_LENGTH 8
+#define PAGE_SHIFT 12 // <-- need to determine by checking kernel source!
+
+unsigned getBCMBase();
+  
+unsigned ceilToPage(unsigned size);
+
+void * initMemMap(unsigned offset, unsigned size);
+
+void clearMemMap(void * map, unsigned size);
+
+void * toUncachedAddr(void * virtAddr, bool useDirectUncached);
+
+void initLockedMem(void * mem, unsigned size);
+
+void clearLockedMem(void * mem, unsigned size);
 
 #endif
