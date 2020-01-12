b706f45c7f3140199185ed0e6d79f01d702c08b1
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2020-01-12 09:27:01 -0800

    _t transition

--- bcm/pimem.c
+++ bcm/pimem.c
@@ -1,8 +1,8 @@
 #include "pimem.h"
 
-static unsigned bcm_base = 0;
-static unsigned memfd = 0;
-static unsigned pagemapfd = 0;
+static uint32_t bcm_base = 0;
+static uint32_t memfd = 0;
+static uint32_t pagemapfd = 0;
 
 // initializes memfd and pagemapfd, which are used by multiple functions in this file
 static bool openFiles() {
@@ -22,41 +22,27 @@ static bool openFiles() {
 }
 
 // used for calculations, not for an actual reference to bcm base, therefore returns an integer instead of a ptr
-unsigned getBCMBase() {
+uint32_t getBCMBase() {
     return bcm_base;
 }
 
-static unsigned ceilToPage(unsigned size) {
+static uint32_t ceilToPage(uint32_t size) {
     return (size & (BCM_PAGESIZE - 1)) ? size + BCM_PAGESIZE - (size & (BCM_PAGESIZE-1)): size;
 }
 
-void * initMemMap(unsigned offset, unsigned size) {
-    int fd;
+void * initMemMap(uint32_t offset, uint32_t size) {
     if (offset % BCM_PAGESIZE) {
         printf("ERROR: the address offset must be a multiple of the page size, which is %d bytes.\n", BCM_PAGESIZE);
         return 0;
     }
-    if (openFiles()) return 0;
-    return mmap(0, size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED|MAP_LOCKED, fd, bcm_base + offset);
+    if (!memfd) {
+        if (openFiles()) return 0;
+    }    
+    return mmap(0, size, PROT_READ|PROT_WRITE|PROT_EXEC, MAP_SHARED|MAP_LOCKED, memfd, bcm_base + offset);
 }
 
-void clearMemMap(void * map, unsigned size) {
+void clearMemMap(void * map, uint32_t size) {
     munmap(map, size);
-}
-
-void * initLockedMem(unsigned size) {
-    int fd;
-    size = ceilToPage(size);
-    void * mem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS|MAP_NORESERVE|MAP_LOCKED, -1, 0);
-    memset(mem, 0, size);
-    mlock(mem, size);
-    
-    return mem;
-}
-
-void clearLockedMem(void * mem, unsigned size) {
-    munlock(mem, size);
-    munmap(mem, size);    
 }
 
 /* thanks to https://shanetully.com/2014/12/translating-virtual-addresses-to-physcial-addresses-in-user-space/
@@ -74,17 +60,32 @@ The latter returns a single void* == the phyiscal address correspoinding to virt
 initUncachedMemView() solves the problem of virtual addresses passing thru the Pi's L1 cache when mapping
 
 */
+
+void * initLockedMem(uint32_t size) {
+    size = ceilToPage(size);
+    void * mem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS|MAP_NORESERVE|MAP_LOCKED, -1, 0);
+    memset(mem, 0, size);
+    mlock(mem, size);
+    
+    return mem;
+}
+
+void clearLockedMem(void * mem, uint32_t size) {
+    munlock(mem, size);
+    munmap(mem, size);    
+}
+
 void initVirtPhysPage(void ** virtAddr, void ** physAddr) {
     if (!pagemapfd) {
         if (openFiles()) return;
     }
 
-    unsigned long pageInfo;
+    uint64_t pageInfo;
     *virtAddr = valloc(BCM_PAGESIZE);
     mlock(*virtAddr, BCM_PAGESIZE);
     memset(*virtAddr, 0, BCM_PAGESIZE);
 
-    if ((lseek(pagemapfd, (unsigned long)*virtAddr/BCM_PAGESIZE * PAGEMAP_LENGTH, SEEK_SET)) < 0) {
+    if ((lseek(pagemapfd, (uint64_t)*virtAddr/BCM_PAGESIZE * PAGEMAP_LENGTH, SEEK_SET)) < 0) {
         printf("Failure to seek page map to proper location.\n");
         return 0;
     }
@@ -102,11 +103,11 @@ void * virtToPhys(void * virtAddr) {
         if (openFiles()) return 0;
     }
 
-    unsigned * pageNumber = (unsigned *) virtAddr / BCM_PAGESIZE;
-    unsigned offset = (unsigned) virtAddr % BCM_PAGESIZE;
+    uint32_t * pageNumber = (uint32_t *) virtAddr / BCM_PAGESIZE;
+    uint32_t offset = (uint32_t) virtAddr % BCM_PAGESIZE;
 
-    unsigned long pageInfo;
-    int err = lseek(pagemapfd, pageNumber*PAGEMAP_LENGTH, SEEK_SET);
+    uint64_t pageInfo;
+    int32_t err = lseek(pagemapfd, pageNumber*PAGEMAP_LENGTH, SEEK_SET);
     if (err != pageNumber*PAGEMAP_LENGTH) {
         printf("WARNING: failed to seek. Expected %i, got %i\n", pageNumber*PAGEMAP_LENGTH, err);
     }
@@ -125,7 +126,7 @@ void * virtToUncachedPhys(void * virtAddr, bool useDirectUncached) {
     return useDirectUncached ? (virtAddr | DIRECT_UNCACHED_BASE) : (virtAddr | L2_COHERENT_BASE);
 }
 
-void * initUncachedMemView(void * virtAddr, unsigned size) {
+void * initUncachedMemView(void * virtAddr, uint32_t size) {
     size = ceilToPage(size);
 
     // allocate arbitrary virtual mem then immediately free it
@@ -145,7 +146,7 @@ void * initUncachedMemView(void * virtAddr, unsigned size) {
     return mem;
 }
 
-void clearUncachedMemView(void * mem, unsigned size) {
+void clearUncachedMemView(void * mem, uint32_t size) {
     size = ceilToPage(size);
     munmap(mem, size);
 }
