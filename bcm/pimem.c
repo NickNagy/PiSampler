c52653d1d60cbfb7f13587e56bfbf9f038b49531
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2020-01-12 09:57:12 -0800

    comments added to functions

--- bcm/pimem.c
+++ bcm/pimem.c
@@ -4,7 +4,10 @@ static uint32_t bcm_base = 0;
 static uint32_t memfd = 0;
 static uint32_t pagemapfd = 0;
 
-// initializes memfd and pagemapfd, which are used by multiple functions in this file
+/* 
+initializes memfd and pagemapfd, which are used by multiple functions in this file
+returns 0 if successful, 1 otherwise
+*/
 static bool openFiles() {
     if (!memfd) {
         if((memfd = open("/dev/mem", O_RDWR | O_SYNC)) < 0) {
@@ -26,10 +29,12 @@ uint32_t getBCMBase() {
     return bcm_base;
 }
 
+/* rounds size to nearest multiple of BCM_PAGESIZE and returns */
 static uint32_t ceilToPage(uint32_t size) {
     return (size & (BCM_PAGESIZE - 1)) ? size + BCM_PAGESIZE - (size & (BCM_PAGESIZE-1)): size;
 }
 
+/* returns a map to memory of given size and @ address offset */
 void * initMemMap(uint32_t offset, uint32_t size) {
     if (offset % BCM_PAGESIZE) {
         printf("ERROR: the address offset must be a multiple of the page size, which is %d bytes.\n", BCM_PAGESIZE);
@@ -61,6 +66,10 @@ initUncachedMemView() solves the problem of virtual addresses passing thru the P
 
 */
 
+/* 
+returns a page of virtual memory that is guaranteed locked in place in RAM and won't be redirected to swap space
+size should be a multiple of BCM_PAGESIZE --> if not, it will be rounded to the nearest BCM_PAGESIZE multiple 
+*/
 void * initLockedMem(uint32_t size) {
     size = ceilToPage(size);
     void * mem = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED|MAP_ANONYMOUS|MAP_NORESERVE|MAP_LOCKED, -1, 0);
@@ -75,34 +84,47 @@ void clearLockedMem(void * mem, uint32_t size) {
     munmap(mem, size);    
 }
 
+/*
+creates a page of virtual memory and updates *virtAddr to point to it. *physAddr updates to point to the corresponding physical address
+of the virtual page
+*/
 void initVirtPhysPage(void ** virtAddr, void ** physAddr) {
     if (!pagemapfd) {
         if (openFiles()) return;
     }
 
     uint64_t pageInfo;
+    
+    // initialize virtual page, lock, clear
     *virtAddr = valloc(BCM_PAGESIZE);
     mlock(*virtAddr, BCM_PAGESIZE);
     memset(*virtAddr, 0, BCM_PAGESIZE);
 
+    // (*virtAddr/BCM_PAGESIZE * PAGEMAP_LENGTH) == offset of pageInfo, so seek to that location in pagemapfd
     if ((lseek(pagemapfd, (uint64_t)*virtAddr/BCM_PAGESIZE * PAGEMAP_LENGTH, SEEK_SET)) < 0) {
         printf("Failure to seek page map to proper location.\n");
         return 0;
     }
 
+    // we are concerned about bits 0 - 54 --> bit 55 is a flag
     read(pagemapfd, &pageInfo, PAGEMAP_LENGTH - 1);
     pageInfo &= PAGE_INFO_MASK;
     
+    // update *physAddr to point to the physical address corresponding to *virtAddr, given by (pageInfo*BCM_PAGESIZE)
     *physAddr = (void*)(pageInfo*BCM_PAGESIZE);
     printf("Virtual page based at %p maps to physical address base %p\n", *virtAddr, *physAddr);
 }
 
-
+/*
+returns a pointer to the corresponding physical address of *virtAddr
+*/
 void * virtToPhys(void * virtAddr) {
     if (!pagemapfd) {
         if (openFiles()) return 0;
     }
-
+    
+    // start w/ same procedure as initVirtPhysPage(). Seek to the region of pagemapfd that will give us the pageInfo
+    
     uint32_t * pageNumber = (uint32_t *) virtAddr / BCM_PAGESIZE;
     uint32_t offset = (uint32_t) virtAddr % BCM_PAGESIZE;
 
@@ -118,24 +140,42 @@ void * virtToPhys(void * virtAddr) {
         printf("WARNING: %p has no phyiscal address!\n", virtAddr);
     }
 
+    // we are concerned about bits 0 - 54 --> bit 55 is a flag
     pageInfo &= PAGE_INFO_MASK;
     return (void *)(pageNumber*BCM_PAGESIZE + offset);
 }
 
+/*
+returns a corresponding physical address ptr for virtAddr that bypasses L1 cache.
+if useDirectUncached == 0, will return an address that also bypasses L2 (direct uncached memory)
+if useDirectUncached == 1, will return an address that is L2 cache-coherent
+*/
 void * virtToUncachedPhys(void * virtAddr, bool useDirectUncached) {
-    return useDirectUncached ? (virtAddr | DIRECT_UNCACHED_BASE) : (virtAddr | L2_COHERENT_BASE);
+    return useDirectUncached ? (virtToPhys(virtAddr) | DIRECT_UNCACHED_BASE) : (virtToPhys(virtAddr) | L2_COHERENT_BASE);
 }
 
-void * initUncachedMemView(void * virtAddr, uint32_t size) {
+/*
+returns a pointer (as a map to uncached mem) that behaves like virtAddr but inherently bypasses L1 cache (virtAddr writes pass thru cache by default)
+size should be a multiple of BCM_PAGESIZE --> if not, it will be rounded to the nearest BCM_PAGESIZE multiple
+if useDirectUncached == 0, will return mem map that also bypasses L2 (direct uncached memory)
+if useDirectUncached == 1, will return mem map that is L2 cache-coherent
+
+WARNINGS: 
+the returned value and original *virtAddr should not be used in conjunction --> I suggest returning value back into virtAddr
+the original memory should not be unmapped during the uncached mem's lifetime
+
+(once again, credit goes to: https://github.com/Wallacoloo/Raspberry-Pi-DMA-Example/blob/master/dma-gpio.c)
+*/
+void * initUncachedMemView(void * virtAddr, uint32_t size, bool useDirectUncached) {
     size = ceilToPage(size);
 
     // allocate arbitrary virtual mem then immediately free it
     void * mem = mmap(NULL, size, PROT_WRITE|PROT_READ, MAP_SHARED|MAP_LOCKED|MAP_ANONYMOUS|MAP_NORESERVE, -1, 0);
     munmap(mem, size);
 
-    // iterate page by page of uncached mem space, verifying that mem is contiguous
+    // iterate page by page of uncached mem space, verifying that mem is contiguous, and mapping/offsetting to uncached addresses
     for (int offset = 0; offset < size; offset += BCM_PAGESIZE) {
-        void * mappedPage = mmap(mem + offset, BCM_PAGESIZE, PROT_WRITE|PROT_READ, MAP_SHARED|MAP_FIXED|MAP_NORESERVE, memfd, virtToPhys(virtAddr + offset));
+        void * mappedPage = mmap(mem + offset, BCM_PAGESIZE, PROT_WRITE|PROT_READ, MAP_SHARED|MAP_FIXED|MAP_NORESERVE, memfd, virtToUncachedPhys(virtAddr + offset, useDirectUncached));
         if (mappedPage != mem + offset) {
             printf("Failed to map contiguous uncached memory.\n");
             return 0;
