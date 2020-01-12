70d47d9e18f876959afdc1ae6aeeab73f65a6e09
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2020-01-12 09:05:10 -0800

    _t transition

--- globals.c
+++ globals.c
@@ -1,12 +1,12 @@
 #include "globals.h"
 
-void * getAlignedPointer(void * ptr, int byteAlignment) {
+void * getAlignedPointer(void * ptr, int32_t byteAlignment) {
     if (!ptr) {
         printf("ERROR! Uninitialized pointer. Aborting without update.\n");
     } else if (byteAlignment % 2 != 0) { // TODO: this logic is incorrect!!
         printf("ERROR! Byte alignment must be a power of 2. Pointer not updated.\n");
     } else {
-        int ptrRelevantBits = (int)ptr & ~(~byteAlignment + 1);
+        int32_t ptrRelevantBits = (int32_t)ptr & ~(~byteAlignment + 1);
         if (!ptrRelevantBits) // already aligned
                 return ptr;
         return (void*)((char *)ptr + (byteAlignment - ptrRelevantBits));
@@ -15,6 +15,6 @@ void * getAlignedPointer(void * ptr, int byteAlignment) {
 }
 
 // from pigpio.c --> https://github.com/joan2937/pigpio/blob/master/pigpio.c
-unsigned gpioDelay(unsigned micros) {
+unsigned gpioDelay(uint32_t micros) {
     return 0;
 }
