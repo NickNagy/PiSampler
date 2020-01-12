19dbd45af4ab17a83052f513d1689afc3d6d22ab
Nick Nagy <33533928+NickNagy@users.noreply.github.com>
2020-01-12 11:38:10 -0800

    _t type

--- wavewriter.c
+++ wavewriter.c
@@ -8,41 +8,43 @@ This file is for reading sample data and saving it to a WAVE file. It will event
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
+#include <bool.h>
 
 #define MAX_BYTES 100000
 
 typedef struct header
 {
     char id[4];// = "RIFF";
-    unsigned int fileSize; //  = 44;
+    uint32_t fileSize; //  = 44;
     char headerFormat[4]; //  = "WAVE";
     char chunkMarkerFormat[4]; //  = "fmt ";
-    unsigned int formatDataLength; // = 16; // # bytes from id to chunkMarkerFormat
-    unsigned short int formatType; // = 1;  // PCM
-    unsigned short int numChannels; // = NUM_CHANNELS;
-    unsigned int sampleRate; // = SAMPLE_RATE;
-    unsigned int byteRate; // = BYTE_RATE;
-    unsigned int monoOrStereo; // = bitsPerSample * numChannels / 8;
-    unsigned int bitsPerSample; // = BITS_PER_SAMPLE;
+    uint32_t formatDataLength; // = 16; // # bytes from id to chunkMarkerFormat
+    uint16_t formatType; // = 1;  // PCM
+    uint16_t numChannels; // = NUM_CHANNELS;
+    uint32_t sampleRate; // = SAMPLE_RATE;
+    uint32_t byteRate; // = BYTE_RATE;
+    uint32_t monoOrStereo; // = bitsPerSample * numChannels / 8;
+    uint32_t bitsPerSample; // = BITS_PER_SAMPLE;
     char dataChunkHeader[4]; // = "data";
-    unsigned int dataSize;
+    uint32_t dataSize;
 } header;
 
 typedef struct wave
 {
     header h;
     char *data; // = (char *)malloc(MAX_BYTES * sizeof(char));
-    long long int index; // = 0;
+    uint64_t index; // = 0;
 } wave;
 
-unsigned char isBigEndian;
-unsigned char reverse;
+bool isBigEndian;
+bool reverse;
 wave * programWave;
 
-void reverseEndian(const long long int size, void *n)
+void reverseEndian(const uint64_t size, void *n)
 {
     char temp;
-    for (long long int i = 0; i < size / 2; i++)
+    for (uint64_t i = 0; i < size / 2; i++)
     {
         temp = ((char *)n)[i];
         ((char *)n)[i] = ((char *)n)[size - (i + 1)];
@@ -52,20 +54,20 @@ void reverseEndian(const long long int size, void *n)
 
 void reverseEndianHeader(header *h)
 {
-    reverseEndian(sizeof(int), (void *)&(h->fileSize));
-    reverseEndian(sizeof(int), (void *)&(h->formatDataLength));
-    reverseEndian(sizeof(short int), (void *)&(h->formatType));
-    reverseEndian(sizeof(short int), (void *)&(h->numChannels));
-    reverseEndian(sizeof(int), (void *)&(h->sampleRate));
-    reverseEndian(sizeof(int), (void *)&(h->byteRate));
-    reverseEndian(sizeof(int), (void *)&(h->monoOrStereo));
-    reverseEndian(sizeof(int), (void *)&(h->bitsPerSample));
-    reverseEndian(sizeof(int), (void *)&(h->dataSize));
+    reverseEndian(4, (void *)&(h->fileSize));
+    reverseEndian(4, (void *)&(h->formatDataLength));
+    reverseEndian(2, (void *)&(h->formatType));
+    reverseEndian(2, (void *)&(h->numChannels));
+    reverseEndian(4, (void *)&(h->sampleRate));
+    reverseEndian(4, (void *)&(h->byteRate));
+    reverseEndian(4, (void *)&(h->monoOrStereo));
+    reverseEndian(4, (void *)&(h->bitsPerSample));
+    reverseEndian(4, (void *)&(h->dataSize));
 }
 
 void readNextData(wave *w, const float *nextSample, const float maxFloat)
 {
-    unsigned int i;
+    uint32_t i;
     if (w->h.dataSize <= (MAX_BYTES - (BITS_PER_SAMPLE/8))) {
         switch (BITS_PER_SAMPLE)
         {
@@ -73,7 +75,7 @@ void readNextData(wave *w, const float *nextSample, const float maxFloat)
         {
             for (i = 0; i < NUM_CHANNELS; i++)
             {
-                long int sample = (long int)(((pow(2, 32 - 1) - 1) / maxFloat) * nextSample[i]);
+                uint64_t sample = (uint64_t)(((pow(2, 32 - 1) - 1) / maxFloat) * nextSample[i]);
                 if (isBigEndian)
                     reverseEndian(4, (void *)&sample);
                 char *sampleChars = (char *)&sample;
@@ -90,7 +92,7 @@ void readNextData(wave *w, const float *nextSample, const float maxFloat)
         {
             for (i = 0; i < NUM_CHANNELS; i++)
             {
-                int sample = (int)((32767 / maxFloat) * nextSample[i]);
+                int32_t sample = (int32_t)((32767 / maxFloat) * nextSample[i]);
                 if (isBigEndian)
                     reverseEndian(2, (void *)&sample);
                 char *sampleChars += (char *)&sample;
@@ -105,7 +107,7 @@ void readNextData(wave *w, const float *nextSample, const float maxFloat)
         {
             for (i = 0; i < NUM_CHANNELS; i++)
             {
-                short int sample = (short int)(127 + (127.0 / maxFloat) * nextSample[i]);
+                int16_t sample = (int16_t)(127 + (127.0 / maxFloat) * nextSample[i]);
                 if (isBigEndian)
                     reverseEndian(1, (void *)&sample);
                 w->data[w->index] += ((char *)&sample)[0];
@@ -120,14 +122,14 @@ void readNextData(wave *w, const float *nextSample, const float maxFloat)
 
 void clear(wave *w)
 {
-    for (unsigned int i = 0; i < w->h.dataSize; i++) {
+    for (uint32_t i = 0; i < w->h.dataSize; i++) {
         w->data[i] = 0;
     }
     w->index = 0;
     w->h.dataSize = 0;
 }
 
-void initializeWave(wave * w, unsigned int const sampleRate, unsigned int const bitsPerSample, unsigned short int const numChannels) {
+void initializeWave(wave * w, uint32_t const sampleRate, uint32_t const bitsPerSample, uint16_t const numChannels) {
     w->h.id = "RIFF";
     w->h.headerFormat = "WAVE";
     w->h.chunkMarkerFormat = "fmt ";
