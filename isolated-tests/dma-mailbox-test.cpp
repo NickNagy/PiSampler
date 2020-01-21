d8c99c46d982e8e4c6b1fd10ab0965a4daecfbf0
Nick <nickvincentnagy@gmail.com>
2020-01-21 12:10:02 -0800

    test 0

--- isolated-tests/dma-mailbox-test.cpp
+++ isolated-tests/dma-mailbox-test.cpp
@@ -18,6 +18,34 @@
 // Memory allocation flags
 #define MEM_ALLOC_FLAG_DIRECT (1 << 2) // Allocate uncached memory that bypasses L1 and L2 cache on loads and stores
 
+// dma
+#define DMA_BASE_OFFSET  0x007000
+#define DMA_MAPSIZE      0x1000
+#define DMA_SINGLE_REG_MAPSIZE 0x100
+
+#define VC_BUS_BASE 0xC0000000
+
+#define DMA_CHANNEL(x) x*64
+
+#define DMA_CS_REG(x)        DMA_CHANNEL(x)
+#define DMA_CONBLK_AD_REG(x) DMA_CHANNEL(x) + 1
+#define DMA_TI_REG(X)        DMA_CHANNEL(x) + 2
+#define DMA_SOURCE_AD_REG(x) DMA_CHANNEL(x) + 3
+#define DMA_DEST_AD_REG(x)   DMA_CHANNEL(x) + 4
+#define DMA_TXFR_LEN_REG(x)  DMA_CHANNEL(x) + 5
+#define DMA_STRIDE_REG(x)    DMA_CHANNEL(x) + 6
+#define DMA_NEXTCONBK_REG(x) DMA_CHANNEL(x) + 7
+#define DMA_DEBUG_REG(x)     DMA_CHANNEL(x) + 8
+
+#define DMA_GLOBAL_ENABLE_REG   0xFF0>>2
+
+// Control status register bits
+#define DMA_RESET 1<<31
+#define DMA_ABORT 1<<30
+
+#define SRC_INC  1<<8
+#define DEST_INC 1<<4
+
 static uint32_t bcm_base = 0;
 static uint32_t memfd = 0;
 static uint32_t pagemapfd = 0;
@@ -49,6 +77,16 @@ struct MailboxMessage
     uint32_t result;
   };
   uint32_t messageEndSentinel;
+};
+
+struct DMAControlBlock {
+  uint32_t transferInfo;
+  uint32_t srcAddr;
+  uint32_t destAddr;
+  uint32_t transferLength;
+  uint32_t stride;
+  uint32_t nextControlBlockAddr;
+  uint64_t reserved;
 };
 
 /* 
@@ -163,8 +201,78 @@ void FreeUncachedGpuMemory(GpuMemory mem)
   Mailbox(MEM_FREE_MESSAGE, mem.allocationHandle);
 }
 
-void dma_test_0() {
+#define DMA_TEST_CHANNEL 5
 
+void dma_test_0() {
+  printf("Mailbox approach:\n");
+    
+  void * virtSrcPage, *busSrcPage;
+  GpuMemory srcPages = AllocateUncachedGpuMemory(PAGE_SIZE);
+  virtSrcPage = srcPages.virtualAddr;
+  busSrcPage = (void *)(srcPages.busAddress);
+
+  DEBUG_PTR("virtSrc", virtSrcPage);
+  DEBUG_PTR("physSrc", busSrcPage);
+
+  void * virtDestPage, *busDestPage;
+  GpuMemory destPages = AllocateUncachedGpuMemory(PAGE_SIZE);
+  virtDestPage = destPages.virtualAddr;
+  busDestPage = (void *)(destPages.busAddress);
+    
+  sleep(1);
+
+  char *srcArr = (char *)virtSrcPage;
+  srcArr[0] = 'h';
+  srcArr[1] = 'e';
+  srcArr[2] = 'l';
+  srcArr[3] = 'l';
+  srcArr[4] = 'o';
+  srcArr[5] = ' ';
+  srcArr[6] = 'w';
+  srcArr[7] = 'o';
+  srcArr[8] = 'r';
+  srcArr[9] = 'l';
+  srcArr[10] = 'd';
+  srcArr[11] = '!';
+  srcArr[12] = 0;
+	
+  void * virtCBPage, *busCBPage;
+  GpuMemory cbPages = initUncachedMemView(PAGE_SIZE);
+  virtCBPage = cbPages.virtAddr;
+  busCBPage = (void*)(cbPages.busAddr);
+
+  DEBUG_PTR("virtCB", virtCBPage);
+  DEBUG_PTR("physCB", busCBPage);
+
+  DMAControlBlock * cb = (DMAControlBlock *)virtCBPage;  
+
+  cb -> transferInfo = SRC_INC | DEST_INC;
+  cb -> srcAddr = (uint32_t)busSrcPage;
+  cb -> destAddr = (uint32_t)busDestPage;
+  cb -> transferLength = 13;
+  cb -> stride = 0;
+  cb -> reserved = 0;
+  cb -> nextControlBlockAddr = 0;
+
+      //enable DMA channel
+	dmaMap[DMA_GLOBAL_ENABLE_REG] |= (1 << DMA_TEST_CHANNEL);
+
+	dmaMap[DMA_CS_REG(DMA_TEST_CHANNEL)] = DMA_RESET;
+	sleep(1);
+
+	// clear debug flags
+	dmaMap[DMA_DEBUG_REG(DMA_TEST_CHANNEL)] = 7; 
+	dmaMap[DMA_CONBLK_AD_REG(DMA_TEST_CHANNEL)] = (uint32_t)busCBPage; // temporarily...
+
+  dmaMap[DMA_CS_REG(DMA_TEST_CHANNEL)] = 1;
+
+  sleep(1);
+
+  printf("destination reads: '%s'\n", (char*)virtDestPage);
+
+  FreeUncachedGpuMemory(srcPages);
+  FreeUncachedGpuMemory(destPages);
+  FreeUncachedGpuMemory(cbPages);
 }
 
 void dma_test_1() {
@@ -172,5 +280,9 @@ void dma_test_1() {
 }
 
 int main() {
+  openFiles();
 
+  dmaMap = initMemMap(DMA_BASE_OFFSET, DMA_MAPSIZE);
+
+  dma_test_0();
 }
